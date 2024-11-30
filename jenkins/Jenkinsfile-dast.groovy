01a190352c0e9a440a3e9c05a7a7fc7fc06f5830
Jetawo Oreoluwa <jetaneore22@gmail.com>
2024-11-30 11:36:41 +0100

    fix results not showing

--- jenkins/Jenkinsfile-dast.groovy
+++ jenkins/Jenkinsfile-dast.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Qualys scan'){
             steps {
-                qualysWASScan authRecord: 'none', cancelHours: '1', cancelOptions: 'xhours', credsId: 'a1be2a3d-86c9-4c2e-a4a0-ac976fd3902f', isSev4Vulns: true, isSev5Vulns: true, optionProfile: 'other', optionProfileId: '2802136', platform: 'EU_PLATFORM_2', pollingInterval: '5', scanName: '[job_name]_jenkins_build_[build_number]', scanType: 'VULNERABILITY', severity4Limit: 2, severity5Limit: 2, vulnsTimeout: '1*60', webAppId: '258412127'
+                qualysWASScan authRecord: 'none', cancelOptions: 'none', credsId: 'a512603e-b571-4169-92a5-62cdd0f517c9', isSev4Vulns: true, isSev5Vulns: true, optionProfile: 'other', optionProfileId: '2802136', platform: 'EU_PLATFORM_2', pollingInterval: '5', scanName: '[job_name]_jenkins_build_[build_number]', scanType: 'VULNERABILITY', severity4Limit: 2, severity5Limit: 2, vulnsTimeout: '60*24', webAppId: '258412127'
             }
         } 
     }
