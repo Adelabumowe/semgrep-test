05d47a8d06af0185ed25bcbc507668d196a1daa2
Jetawo Oreoluwa <jetaneore22@gmail.com>
2024-11-30 14:54:58 +0100

    fix results not showing

--- jenkins/Jenkinsfile-dast.groovy
+++ jenkins/Jenkinsfile-dast.groovy
@@ -9,7 +9,7 @@ pipeline {
         }
         stage('Qualys scan'){
             steps {
-                qualysWASScan authRecord: 'none', cancelOptions: 'none', credsId: 'a512603e-b571-4169-92a5-62cdd0f517c9', isSev4Vulns: true, isSev5Vulns: true, optionProfile: 'other', optionProfileId: '2802136', platform: 'EU_PLATFORM_2', pollingInterval: '5', scanName: '[job_name]_jenkins_build_[build_number]', scanType: 'VULNERABILITY', severity4Limit: 2, severity5Limit: 2, vulnsTimeout: '60*24', webAppId: '258412127'
+                qualysWASScan authRecord: 'none', cancelOptions: 'none', credsId: 'a512603e-b571-4169-92a5-62cdd0f517c9', isSev1Vulns: true, isSev2Vulns: true, isSev3Vulns: true, isSev4Vulns: true, isSev5Vulns: true, optionProfile: 'other', optionProfileId: '2802136', platform: 'EU_PLATFORM_2', pollingInterval: '5', scanName: '[job_name]_jenkins_build_[build_number]', scanType: 'VULNERABILITY', severity1Limit: 2, severity2Limit: 3, severity3Limit: 2, severity4Limit: 2, severity5Limit: 2, vulnsTimeout: '2*60', webAppId: '258412127'
             }
         } 
     }
